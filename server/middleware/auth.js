7e326896a8bc51e6d5b612be784ba87a57e115b5
Jeffrey Chen <jeffreychen41@gmail.com>
2017-05-27 12:01:24 -0700

    Added redis to go

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,7 +1,18 @@
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
-const redisClient = require('redis').createClient(process.env.REDIS_URL);
-const redisLocal = require('redis').createClient();
+const redisClient = require('redis').createClient(process.env.REDISTOGO_URL);
+
+
+if (process.env.REDISTOGO_URL) {
+    // TODO: redistogo connection
+    var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+    var redis = require("redis").createClient(rtg.port, rtg.hostname);
+
+redis.auth(rtg.auth.split(":")[1]);
+} else {
+    var redis = require("redis").createClient();
+}
+
 module.exports.verify = (req, res, next) => {
   if (req.isAuthenticated()) {
     return next();
