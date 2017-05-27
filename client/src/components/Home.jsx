c0a8b84ebc31409e450c87eddbc30043780c2115
Jon Eric Escobedo <jonericescobedo@gmail.com>
2017-05-26 21:20:37 -0700

    (frontend) Change navbar and udpate mobile styling

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+// import RaisedButton from 'material-ui/RaisedButton';
+
+const style = {
+  margin: 12,
+};
 
 const HomePage = () => (
   <div>
-    <h1>BestFit HomePage</h1>
-    <p>Your one stop autoshop for your job search</p>
+
+    <div className="row vert-center">
+      <div className="col text-center align-self-center">
+        <h1 className="display-1">BestFit</h1>
+         <p className="text-muted">Your one stop autoshop for your job search</p>
+         <button type="button" className="btn btn-primary">Signup</button>
+         <button type="button" className="btn btn-primary">Guest</button>
+
+      </div>
+    </div>
   </div>
 );
 
