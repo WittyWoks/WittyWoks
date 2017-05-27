ee2d84dc173cc735bbba55407c2fd5c9801900a9
Jeffrey Chen <jeffreychen41@gmail.com>
2017-05-27 12:19:53 -0700

    Fixed PORT

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@
 const app = require('./app');
 const db = require('../db');
 const path = require('path');
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log('Example app listening on port 3000!');
