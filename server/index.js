e0bbd1fc06b829cc3a32e6f2211897da49228b9c
Jeffrey Chen <jeffreychen41@gmail.com>
2017-05-27 14:55:09 -0700

    Added Google Auth

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.listen(PORT, () => {
   console.log('Example app listening on port 3000!');
 });
 
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 app.get('/dashboard', (req, res) => {
