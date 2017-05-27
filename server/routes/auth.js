9e960124d229fbe18a0240ef489d450ef915d262
Jeffrey Chen <jeffreychen41@gmail.com>
2017-05-27 10:38:36 -0700

    Staging for rebase

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,33 +1,8 @@
 const express = require('express');
 const middleware = require('../middleware');
 const router = express.Router();
-const app = express();
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/../react-client/dist/index.html'));
-});
-
-app.get('/dashboard', (req, res) => {
-  res.sendFile(path.join(__dirname, '/../react-client/dist/index.html'));
-})
 
 router.route('/')
-  .get(middleware.auth.verify, (req, res) => {
-    res.redirect('/dashboard');
-  });
-
-router.route('/login')
-  .get((req, res) => {
-    res.render('login.ejs', { message: req.flash('loginMessage') });
-  })
-  .post(middleware.passport.authenticate('local-login', {
-    successRedirect: '/profile',
-    failureRedirect: '/login',
-    failureFlash: true
-  }));
-
-
-router.route('/profile')
   .get(middleware.auth.verify, (req, res) => {
     res.redirect('/dashboard');
   });
@@ -43,7 +18,7 @@ router.get('/auth/google', middleware.passport.authenticate('google', {
 }));
 
 router.get('/auth/google/callback', middleware.passport.authenticate('google', {
-  successRedirect: '/profile',
+  successRedirect: '/dashboard',
   failureRedirect: '/login'
 }));
 
