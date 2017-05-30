be0ff6cb32bc97ffeef7c1bbb2a5d6be01662429
Jeffrey Chen <jeffreychen41@gmail.com>
2017-05-30 13:58:29 -0700

    Staging for DB deployment

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,8 @@
 const config = require('config');
 
+if (process.env.DATABASE_URL) {
+  config['knex'].connection.database = process.env.DATABASE_URL;
+};
+
 module.exports = config['knex'];
 console.log(config['knex']);
