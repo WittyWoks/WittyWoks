f5ca34b40c4f2367e8b43fd8f7fe899a69381e00
Jeffrey Chen <jeffreychen41@gmail.com>
2017-05-30 17:32:02 -0700

    Deployment db adjustment

--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,4 @@
 const config = require('config');
 
-if (process.env.DATABASE_URL) {
-  config['knex'].connection.database = process.env.DATABASE_URL;
-};
-
 module.exports = config['knex'];
 console.log(config['knex']);
