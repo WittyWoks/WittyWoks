b92c272311e3faa17e3f82b4b1f8831b251fc6e4
Jeffrey Chen <jeffreychen41@gmail.com>
2017-05-30 21:03:28 -0700

    Fixed OAUTH and google login, fixed database save of users

--- server/index.js
+++ server/index.js
@@ -5,8 +5,21 @@ const path = require('path');
 const PORT = process.env.PORT || 3000;
 const formidable = require('formidable'); // JEE ADDED
 const fs = require('fs'); // JEE ADDED
+const pg = require('pg');
 
+const connectionString = process.env.DATABASE_URL || 'postgres://adecthmzqqxcep:903992ed3f10951126eb761b5d9b08197c9adb0316b855f7d8066aa5631bacbf@ec2-54-163-253-94.compute-1.amazonaws.com:5432/d2ms3id7tgktug?ssl=true&amp;sslfactory=org.postgresql.ssl.NonValidatingFactory';
 
+const client = new pg.Client({
+  database: process.env.DATABASE_DB || require('../config/development.json').postgresql.DATABASE_DB,
+  user: process.env.DATABASE_USER || require('../config/development.json').postgresql.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD || require('../config/development.json').postgresql.DATABASE_PASSWORD,
+  host: process.env.DATABASE_HOST || require('../config/development.json').postgresql.DATABASE_HOST,
+  port: 5432,
+  ssl: true,
+  url: process.env.DATABASE_URL || require('../config/development.json').postgresql.DATABASE_URL
+});
+
+client.connect();
 
 app.listen(PORT, () => {
   console.log('Example app listening on port 3000!');
