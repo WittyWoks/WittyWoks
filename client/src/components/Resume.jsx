3a18c8093f6a7974a84e89195041f2b45be07d78
Jon Eric Escobedo <jonericescobedo@gmail.com>
2017-05-30 23:17:31 -0700

    (feat) File upload sending file to local 'uploads' directory

--- client/src/components/Resume.jsx
+++ client/src/components/Resume.jsx
@@ -1,14 +1,59 @@
 import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const styles = {
+  button: {
+    margin: 12,
+  },
+  exampleImageInput: {
+    cursor: 'pointer',
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    width: '100%',
+    opacity: 0,
+  },
+};
 
 class Resume extends React.Component {
   constructor(props) {
     super(props);
   }
 
+
+  fileUpload(e) {
+    // Get file info
+    let file = e.target.files[0];
+    let formData = new FormData();
+    formData.append('upload', file, file.name);
+
+    console.log('triggered', file, file.name);
+    $.ajax({
+      url: '/fileUpload',
+      type: 'POST',
+      data: formData,
+      processData: false,
+      contentType: false,
+      success: function(data) {
+        console.log('made it', data);
+      }
+    });
+  }
+
   render() {
     return (
       <div>
         <h1>Hello from Resume Component</h1>
+        <RaisedButton
+          label="Upload Résumé"
+          labelPosition="before"
+          style={styles.button}
+          containerElement="label"
+        >
+          <input type="file" name="upload" style={styles.exampleImageInput} onChange={(e) => this.fileUpload(e)} />
+        </RaisedButton>
       </div>
     );
   }
--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ const app = require('./app');
 const db = require('../db');
 const path = require('path');
 const PORT = process.env.PORT || 3000;
+const formidable = require('formidable'); // JEE ADDED
+const fs = require('fs'); // JEE ADDED
+
 
 
 app.listen(PORT, () => {
@@ -26,5 +29,33 @@ app.get('/companyInfo', (req, res) => {
 
 app.get('/home', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
+// JEE ADDED
+app.post('/fileUpload', (req, res) => {
+
+  // create an incoming form object
+  var form = new formidable.IncomingForm();
+
+  // store all uploads in the /uploads directory
+  form.uploadDir = path.join(__dirname, '../uploads');
+
+  // every time a file has been uploaded successfully,
+  // rename it to it's orignal name
+  form.on('file', function(field, file) {
+    fs.rename(file.path, path.join(form.uploadDir, file.name));
+  });
+
+  // log any errors that occur
+  form.on('error', function(err) {
+    console.log('An error has occured: \n' + err);
+  });
+
+  // once all the files have been uploaded, send a response to the client
+  form.on('end', function() {
+    res.end('File uploaded');
+  });
+
+  // parse the incoming request containing the form data
+  form.parse(req);
 });
