ddfebd6aaaf059e550665ef1690d13e132e21499
Jon Eric Escobedo <jonericescobedo@gmail.com>
2017-06-01 14:34:00 -0700

    (feat) resume component now displays skills and uploaded resume

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const path = require('path'); // JC ADDED
 const formidable = require('formidable'); // JEE ADDED
 const fs = require('fs'); // JEE ADDED
+const pdfParser = require('../pdfparse.js'); // BB ADDED
 
 router.route('/')
   .get((req, res) => {
@@ -22,9 +23,44 @@ router.route('/home')
 
 router.route('/fileUpload')
   .post((req, res) => {
+    // // create an incoming form object
+    // var form = new formidable.IncomingForm();
+
+    // // parse the incoming request containing the form data
+    // form.parse(req);
+
+    // let getSkills = new Promise((resolve, reject) => {
+    //   let fileName;
+      
+    //   // store all uploads in the /uploads directory
+    //   form.uploadDir = path.join(__dirname, '../../uploads');
+
+    //   // every time a file has been uploaded successfully,
+    //   // rename it to it's orignal name
+    //   form.on('file', function(field, file) {
+    //     fileName = file.name;
+    //     fs.rename(file.path, path.join(form.uploadDir, file.name));
+
+    //     // This is an async call - resolve once this function has completed!
+    //     resolve(fileName);
+    //   });
+
+    //   // log any errors that occur
+    //   form.on('error', function(err) {
+    //     console.log('An error has occured: \n' + err);
+    //   });
+    // })
+    // getSkills.then((fileName) => {
+    //   pdfParser.parsePDF(fileName, function(skills) {
+    //     res.json(skills);
+    //   });
+    // })
     // create an incoming form object
     var form = new formidable.IncomingForm();
 
+    // parse the incoming request containing the form data
+    form.parse(req);
+      
     // store all uploads in the /uploads directory
     form.uploadDir = path.join(__dirname, '../../uploads');
 
@@ -32,25 +68,17 @@ router.route('/fileUpload')
     // rename it to it's orignal name
     form.on('file', function(field, file) {
       fs.rename(file.path, path.join(form.uploadDir, file.name));
-    });
-
-    form.on('progress', function(bytesReceived, bytesExpected) {
-      var percent_complete = (bytesReceived / bytesExpected) * 100;
-      console.log(percent_complete.toFixed(2));
+      pdfParser.parsePDF(file.name, function(skills) {
+        res.json(skills);
+      });
     });
 
     // log any errors that occur
     form.on('error', function(err) {
       console.log('An error has occured: \n' + err);
     });
-
-    // once all the files have been uploaded, send a response to the client
-    form.on('end', function() {
-      res.end('File uploaded');
-    });
-
-    // parse the incoming request containing the form data
-    form.parse(req);
-  });  
+    
+    
+  });
 
 module.exports = router;
