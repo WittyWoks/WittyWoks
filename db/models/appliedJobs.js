be03dafea29f6059b5ad7aae7277c537705264d6
Jeffrey Chen <jeffreychen41@gmail.com>
2017-06-01 16:37:58 -0700

    Added gmail search, prep for database edits

---
+++ db/models/appliedJobs.js
@@ -0,0 +1,10 @@
+const db = require('../');
+
+const appliedJobs = db.Model.extend({
+  tableName: 'appliedJobs',
+  auths: function() {
+    return this.hasMany('Auth');
+  }
+});
+
+module.exports = db.model('appliedJobs', appliedJobs);
