044fd413869c42c2637bf34e6537f55a2b5a3de1
Airyque Ervin <airyque@gmail.com>
2017-06-03 12:22:38 -0700

    hid extra buttons added some stats to jobs

--- client/src/components/CompanyInfo.jsx
+++ client/src/components/CompanyInfo.jsx
@@ -35,21 +35,35 @@ class CompanyInfo extends React.Component {
   render() {
     const jobInfo = this.props.location.state;
     return (
-      <div className="container-fluid">
-        {this.state.jobs.length ? this.state.jobs.map(job => {
-          return <div className="card" key={Math.random() * 1000}>
-            <div className="card-img-top">
-              <img src={job.squareLogo} className="image-fluid card-img-top" alt="Card image cap" />
-            </div>
-            <div className="card-block text-center">
-              <h2 className="media-heading">{job.name}</h2>
-              <h4>{jobInfo.jobtitle}</h4>
-              <p>{jobInfo.snippet}</p>
-              <a className="btn btn-info" role="button" href={`${jobInfo.url}`} target="blank">Apply Now!</a>
-            </div>
-          </div>;
-        }) : null}
-    
+      <div className="card-block">
+        <div className="container-fluid">
+          
+          {/* Single Job Listing */}
+          <div className="row">
+            {this.state.jobs.length ? this.state.jobs.map((job, i) => {
+              if (i === 0) {
+                return <div className="col-md-4 col-md-offset-5" key={Math.random() * 1000}>
+                  <div className="card">
+                    <img src={job.squareLogo} className="image-fluid card-img-top" alt="Card image cap" />
+                  </div>
+                  <div className="card-block text-center">
+                    <h2 className="media-heading">{job.name}</h2>
+                    <h4>{jobInfo.jobtitle}</h4>
+                    <p>{jobInfo.snippet}</p>
+                    <a className="btn btn-info" role="button" href={`${jobInfo.url}`} target="blank">Apply Now!</a>
+                  </div>
+
+                  {/* Company Ratings */}
+                  <div className="row">
+                    <div className="col-md-4">Overall Rating: <strong>{job.overallRating}</strong></div>
+                    <div className="col-md-4">Work Life Balance: <strong>{job.workLifeBalanceRating}</strong></div>
+                    <div className="col-md-4">Culture and Values: <strong>{job.cultureAndValuesRating}</strong></div>
+                  </div>
+                </div>;
+              }
+            }) : null}
+          </div>
+        </div>
       </div>
     );
   }
