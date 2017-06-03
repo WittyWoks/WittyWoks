4a51d8d3a154276f651556ad46fb52a378b75e0b
Jeffrey Chen <jeffreychen41@gmail.com>
2017-06-02 19:22:12 -0700

    Smalled changes to api

--- server/routes/api.js
+++ server/routes/api.js
@@ -11,7 +11,6 @@ const request = require('request'); // AE ADDED
 const aws = require('aws-sdk'); // BB ADDED
 const S3_BUCKET = 'resumeswittywoks'; //BB ADDED
 const userInfo = require('../middleware/passport');
-const userInfo = require('../middleware/passport')
 
 
 router.route('/')
