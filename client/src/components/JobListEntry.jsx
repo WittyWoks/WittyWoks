282a4386f039f341fad90a47221319d4d6494520
Airyque Ervin <airyque@gmail.com>
2017-06-05 12:00:55 -0700

    display relative time to how long ago a job was posted

--- client/src/components/JobListEntry.jsx
+++ client/src/components/JobListEntry.jsx
@@ -6,7 +6,7 @@ const JobListEntry = ({ job }) => (
     <div className="list-group-item list-group-item-action flex-column align-items-start">
       <div className="d-flex w-100 justify-content-between">
         <h5 className="mb-1">{job.company}</h5>
-        <small>3 days ago</small>
+        <small>{job.formattedRelativeTime}</small>
       </div>
       <p className="mb-1">{job.jobtitle}</p>
       <small>{job.city}</small>
@@ -14,14 +14,4 @@ const JobListEntry = ({ job }) => (
 </Link>
 );
 
-{/*
-<Link to={{pathname: `/companyInfo/${job.company}`, state: job}}>
-  <div className="col-sm-6 col-md-4">
-    {job.company} <br/>
-    {job.jobtitle} - {job.city}
-  </div>
-</Link>
-*/}
-
 export default JobListEntry;
-
