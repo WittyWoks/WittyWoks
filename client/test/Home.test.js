6cd9309f0cad8c409ed9997cd41c487069e798eb
Airyque Ervin <airyque@gmail.com>
2017-06-10 15:03:14 -0700

    fixed resume, home, and link tests with snapshots

--- client/test/Home.test.js
+++ client/test/Home.test.js
@@ -4,10 +4,8 @@ import Home from '../../client/src/components/Home.jsx';
 
 describe('A suite to test the Home component', () => {
   it('Expect Home.jsx to render without throwing an error', () => {
-    expect(shallow(<Home />).contains(<h1 className="display-1">BestFit</h1>)).toBe(true);
+    expect(shallow(<Home />).contains(<h1 className="display-1 hero wow fadeInDown" data-wow-delay="0.2s">BestFit</h1>)).toBe(true);
+    
   });
 
-
-})
-
-
+});
