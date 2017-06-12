3368b33a099240e23bd81f19760bc42ccf47b6c0
Jon Eric Escobedo <jonericescobedo@gmail.com>
2017-06-12 14:53:58 -0700

    (rebase) rebase for new PR

--- client/src/components/SmartAnalysis.jsx
+++ client/src/components/SmartAnalysis.jsx
@@ -1,11 +1,115 @@
 import React from 'react';
 import axios from 'axios';
 import DonutChart from './C3Components/DonutChart.jsx';
+import {Tabs, Tab} from 'material-ui/Tabs';
+import LinearProgress from 'material-ui/LinearProgress';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+
+const styles = {
+  headline: {
+    fontSize: 24,
+    paddingTop: 16,
+    marginBottom: 12,
+    fontWeight: 400,
+  },
+  check: {
+    color: '#01B4A0'
+  },
+  cross: {
+    color: '#5596E6'
+  },
+  rating: {
+    fontWeight: 200,
+    fontSize: '0.8em'
+  },
+  cardTitle: {
+    background: '#607d8b'
+  },
+  tab: {
+    background: '#8eacbb'
+  },
+  inkBar: {
+    background: '#d500f9'
+  },
+  tabBody: {
+    background: '#F5F5F6'
+  }
+};
 
 class SmartAnalysis extends React.Component {
   constructor(props) {
     super(props);
     this.fetchAllAppliedJob();
+    this.state = {
+      consumptionPreferences: null,
+      needs: null,
+      personality: null, 
+      values: null,
+      summary: null,
+      likely: [],
+      unlikely: [],
+      emotionTones: null,
+      languageTones: null,
+      socialTones: null,
+      value: 'a'
+    }
+    this.getResumeForWatson();
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  getResumeForWatson() {
+    let context = this;
+    $.ajax({
+      type: 'GET',
+      url: '/user',
+      datatype: 'json'
+    })
+    .done((user) => {
+      $.ajax({
+        url: '/getResume',
+        type: 'GET',
+        data: {resume_id: user.resume_id},
+      })
+      .done((resume) => {
+        axios.get('/analyzeResume', {
+          params: {
+            url: resume.resume_url 
+          }
+        })
+        .then((personality) => {
+          console.log('PERSONALITY', personality);
+          let consumption = personality.data.personality.main.consumption_preferences;
+          for (let i = 0; i < consumption.length; i++) {
+            let category = consumption[i].consumption_preference_category_id;
+            for (let j = 0; j < consumption[i].consumption_preferences.length; j++) {
+              if (consumption[i].consumption_preferences[j].score === 1) {
+                context.state.likely.push(consumption[i].consumption_preferences[j].name.substring(9));
+              } else if (consumption[i].consumption_preferences[j].score === 0) {
+                context.state.unlikely.push(consumption[i].consumption_preferences[j].name.substring(9));
+              }
+            }
+          }
+
+          context.setState({
+            needs: personality.data.personality.main.needs,
+            personality: personality.data.personality.main.personality, 
+            values: personality.data.personality.main.values,
+            summary: personality.data.personality.summary,
+            emotionTones: personality.data.tone.main['document_tone']['tone_categories'][0].tones,
+            languageTones: personality.data.tone.main['document_tone']['tone_categories'][1].tones,
+            socialTones: personality.data.tone.main['document_tone']['tone_categories'][2].tones,
+            loaded: true
+          });
+        });
+      })
+      .fail(function(err) {
+        console.log('failed to GET', err);
+      });
+    })
+    .fail(function(err) {
+      console.log('failed to GET', err);
+    });
   }
 
   fetchAllAppliedJob() {
@@ -142,12 +246,141 @@ class SmartAnalysis extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello from SmartAnalysis Component</h1>
-        <h4>Watson Tone Analyzer</h4>
-        <p>All the content goes here</p>
-        <button>Analyze</button>
         <h4>Keywords tracker</h4>
         {this.state.donutChart ?<DonutChart ranking={this.state.keywordsRanking} /> : null}
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-sm-6">
+              <Card>
+                <CardTitle title="IBM Watson Analysis" subtitle="Smart analysis based on your resume" style={styles.cardTitle} titleColor="#000000" subtitleColor="#000000"/>
+                <CardMedia>
+                  <Tabs
+                    value={this.state.value}
+                    onChange={this.handleChange}
+                    tabItemContainerStyle={styles.tab}
+                    inkBarStyle={styles.inkBar}
+                  >
+                    <Tab label="Summary" value="a">
+                      <div className="container-fluid">
+                        <h4 style={styles.headline}>Your Personality Analysis</h4>
+                        {this.state.loaded === false ?
+                          <p>Loading...</p>
+                        :
+                        <div>
+                          <p>{this.state.summary}</p>
+                          <h4>You are likely to:</h4>
+                          {this.state.likely.map((like) => {
+                            return (
+                              <span><i className="fa fa-check-circle-o" aria-hidden="true" style={styles.check}></i> {like}<br/></span>
+                            );
+                          })}
+                          <h4>You are unlikely to:</h4>
+                          {this.state.unlikely.map((dislike) => {
+                            return (
+                              <span><i className="fa fa-times-circle-o" aria-hidden="true" style={styles.cross}></i> {dislike}<br/></span>
+                            );
+                          })}
+                          
+                        </div>
+                        }
+                      </div>
+                    </Tab>
+                    <Tab label="Tone" value="b">
+                      <div className="container-fluid">
+                        <h4 style={styles.headline}>Resume Tone</h4>
+                        {this.state.loaded === false ?
+                          <p>Loading...</p>
+                        :
+                        <div>
+                          <h6> &#60; .5 = not likely present</h6>
+                          <h6> &#62; .5 = likely present</h6>
+                          <h6> &#62; .75 = very likely present </h6>
+                          <div className="row">
+                            <div className="col-sm-4">
+                              <h4>Emotions</h4>
+                              {this.state.emotionTones.map((emotion) => {
+                                return (
+                                  <p><LinearProgress mode="determinate" value={emotion.score * 100} />{emotion.tone_name}: {emotion.score.toFixed(2)} <span style={styles.rating}>{emotion.score.toFixed(2) < 0.5 ? 'NOT LIKELY' : emotion.score.toFixed(2) > 0.75 ? 'VERY LIKELY' : 'LIKELY'}</span></p>
+                                );
+                              })}
+                            </div>
+                            <div className="col-sm-4">
+                              <h4>Language Style</h4>
+                              {this.state.languageTones.map((emotion) => {
+                                return (
+                                  <p><LinearProgress mode="determinate" value={emotion.score * 100} />{emotion.tone_name}: {emotion.score.toFixed(2)} <span style={styles.rating}>{emotion.score.toFixed(2) < 0.5 ? 'NOT LIKELY' : emotion.score.toFixed(2) > 0.75 ? 'VERY LIKELY' : 'LIKELY'}</span></p>
+                                );
+                              })}
+                            </div>
+                            <div className="col-sm-4">
+                              <h4>Social Tendencies</h4>
+                              {this.state.socialTones.map((emotion) => {
+                                return (
+                                  <p><LinearProgress mode="determinate" value={emotion.score * 100} />{emotion.tone_name}: {emotion.score.toFixed(2)} <span style={styles.rating}>{emotion.score.toFixed(2) < 0.5 ? 'NOT LIKELY' : emotion.score.toFixed(2) > 0.75 ? 'VERY LIKELY' : 'LIKELY'}</span></p>
+                                );
+                              })}
+                            </div>
+                          </div>
+                        </div>
+                        }
+                      </div>
+                    </Tab>
+                    <Tab label="Portrait" value="c">
+                      <div className="container-fluid">
+                        <h4 style={styles.headline}>Personality Portrait</h4>
+                        {this.state.loaded === false ?
+                          <p>Loading...</p>
+                        :
+                        <div>
+                          <div className="row">
+                            <div className="col-sm-4">
+                              <h4>Needs</h4>
+                              {this.state.needs.map((trait) => {
+                                return (
+                                  <div>
+                                    <p><LinearProgress mode="determinate" value={trait.percentile * 100} />{trait.name}: {Math.floor(trait.percentile * 100)}%</p>
+                                  </div>
+                                );
+                              })}
+                            </div>
+
+                            <div className="col-sm-4">
+                              <h4>Personality</h4>
+                              {this.state.personality.map((trait) => {
+                                return (
+                                  <div>
+                                    <p><LinearProgress mode="determinate" value={trait.percentile * 100} />{trait.name}: {Math.floor(trait.percentile * 100)}%</p>
+                                  </div>
+                                );
+                              })}
+                            </div>
+
+                            <div className="col-sm-4">
+                              <h4>Values</h4>
+                              {this.state.values.map((trait) => {
+                                return (
+                                  <div>
+                                    <p><LinearProgress mode="determinate" value={trait.percentile * 100} />{trait.name}: {Math.floor(trait.percentile * 100)}%</p>
+                                  </div>
+                                );
+                              })}
+                            </div>
+
+                          </div>
+                        </div>
+                        }
+                      </div>
+                    </Tab>
+                  </Tabs>
+                </CardMedia>
+                
+                <CardText>
+
+                </CardText>
+              </Card>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- server/routes/resume.js
+++ server/routes/resume.js
@@ -3,6 +3,10 @@ const express = require('express');
 const router = express.Router();
 const pdfParser = require('../pdfparse.js'); // BB ADDED
 const ResumeController = require('../controllers').Resume;
+const PersonalityInsightsV3 = require('watson-developer-cloud/personality-insights/v3');
+const PersonalityTextSummaries = require('personality-text-summary');
+const IBM_USER = process.env.IBM_USER || require('../../config/development.json').IBM.IBM_USER;
+const IBM_PASS = process.env.IBM_PASS || require('../../config/development.json').IBM.IBM_PASS;
 
 router.route('/fileUpload')
   .post((req, res) => {
@@ -28,9 +32,35 @@ router.route('/getResume')
 router.route('/analyzeResume')
   .get((req, res) => {
     let pdfUrl = req.query.url;
+    let v3EnglishTextSummaries = new PersonalityTextSummaries({ locale: 'en', version: 'v3' });
+    let personalitySummary = null;
 
     pdfParser.pdfToText(pdfUrl, (rawText) => {
-      res.json(rawText);
+      console.log(rawText);
+      // Watson init
+      let personality_insights = new PersonalityInsightsV3({
+        username: IBM_USER,
+        password: IBM_PASS,
+        version_date: '2017-05-26'
+      });
+
+      // Watson send text
+      personality_insights.profile({
+        text: rawText,
+        consumption_preferences: true
+      },
+      function (err, response) {
+        if (err) {
+          console.log('error:', err);
+        } else {
+          // console.log(JSON.stringify(response, null, 2));
+          personalitySummary = v3EnglishTextSummaries.getSummary(response);
+          response.summary = personalitySummary;
+          res.json(response);
+        }
+      });
+
+      // res.json(rawText);
     });
 
   });
