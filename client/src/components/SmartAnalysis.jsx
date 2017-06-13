cd4d91c336ee4a534086d17151712106e65301f7
Jon Eric Escobedo <jonericescobedo@gmail.com>
2017-06-13 13:59:50 -0700

    (feat) rebase with keywords PR and implement Watson API calls

--- client/src/components/SmartAnalysis.jsx
+++ client/src/components/SmartAnalysis.jsx
@@ -27,7 +27,7 @@ const styles = {
     background: '#607d8b'
   },
   tab: {
-    background: '#8eacbb'
+    background: '#607d8b'
   },
   inkBar: {
     background: '#d500f9'
@@ -40,8 +40,15 @@ const styles = {
 class SmartAnalysis extends React.Component {
   constructor(props) {
     super(props);
-    this.fetchAllAppliedJob();
     this.state = {
+      loaded: false,
+      barChartDates: null,
+      barChartJobsApplied: null,
+      jobsAppliedTo: null,
+      allData: [],
+      keywordsRanking: null,
+      donutChart: false,
+      skills: [],
       consumptionPreferences: null,
       needs: null,
       personality: null, 
@@ -53,12 +60,131 @@ class SmartAnalysis extends React.Component {
       languageTones: null,
       socialTones: null,
       value: 'a'
-    }
-    this.getResumeForWatson();
+    };
+    this.fetchAllAppliedJob();
+    this.analyzeResume();
     this.handleChange = this.handleChange.bind(this);
   }
 
-  getResumeForWatson() {
+  handleChange(value) {
+    this.setState({
+      value: value,
+    });
+  } 
+
+  fetchAllAppliedJob() {
+    this.getResume();
+    let context = this;
+    $.ajax({
+      type: 'GET',
+      url: '/user',
+      datatype: 'json'
+    })
+    .done(data => {
+      if (data.email) {
+        axios.get('/ReturnJobsApplied', {
+          params: {
+            google_id: data.id
+          }
+        })
+        .then((jobs) => {
+
+          // console.log('Success getting jobs!', jobs);
+          context.setState({
+            allData: jobs.data
+          });
+          let datesObj = {};
+          let yAxis = ['Applied'];
+          let xAxis = [];
+
+          jobs.data.forEach((job) => {
+            let convertedDate = (new Date(job.created_at)).toDateString();
+            if (!datesObj[convertedDate]) {
+              datesObj[convertedDate] = 1;
+            } else {
+              datesObj[convertedDate] += 1;
+            }
+          });
+
+          for (let key in datesObj) {
+            yAxis.push(datesObj[key]);
+            xAxis.push(key);
+          }
+
+          // console.log(xAxis);
+          // console.log(yAxis);
+          context.setState({
+            barChartDates: xAxis,
+            barChartJobsApplied: yAxis,
+            jobsAppliedTo: jobs.data,
+          });
+
+
+          this.urlParser(context.state.allData, this.state.skills);
+        })
+        .catch(err => {
+          console.error('Error occured getting jobs', err);
+        });
+      }
+    });
+  }
+
+  urlParser(DataArray, skills) {
+    let context = this;
+    $.ajax({
+      type: 'POST',
+      url: '/urlParser',
+      contentType: 'application/JSON',
+      data: JSON.stringify({
+        data: DataArray,
+        skills: skills
+      })
+    })
+    .done((ranking) => {
+      ranking = JSON.parse(ranking);
+      context.setState({
+        keywordsRanking: ranking
+      });
+      if (context.state.keywordsRanking !== null) {
+        context.setState({
+          donutChart: true
+        });
+      }
+    })
+    .fail(err => {
+      console.error('Error occured getting jobs', err);
+    });
+  }
+
+  getResume() {
+    let context = this;
+    $.ajax({
+      type: 'GET',
+      url: '/user',
+      datatype: 'json'
+    })
+    .done((user) => {
+      $.ajax({
+        url: '/getResume',
+        type: 'GET',
+        data: {resume_id: user.resume_id},
+      })
+      .done((resume) => {
+        context.setState({
+          skills: resume.skills.split(','),
+          file: resume.resume_url
+        });
+      })
+      .fail(function(err) {
+        console.log('failed to GET', err);
+      });
+    })
+    .fail(function(err) {
+      console.log('failed to GET', err);
+    });
+  }
+
+  analyzeResume() {
     let context = this;
     $.ajax({
       type: 'GET',
@@ -112,144 +238,24 @@ class SmartAnalysis extends React.Component {
     });
   }
 
-  fetchAllAppliedJob() {
-    this.getResume();
-    this.state = {
-      barChartDates: null,
-      barChartJobsApplied: null,
-      jobsAppliedTo: null,
-      loaded: false,
-      allData: [],
-      keywordsRanking:null,
-      donutChart: false,
-      skills:[]
-    };
-    let context = this;
-    $.ajax({
-      type: 'GET',
-      url: '/user',
-      datatype: 'json'
-    })
-    .done(data => {
-      if (data.email) {
-        axios.get('/ReturnJobsApplied', {
-          params: {
-            google_id: data.id
-          }
-        })
-        .then((jobs) => {
-
-          // console.log('Success getting jobs!', jobs);
-          context.setState({
-            allData : jobs.data
-          });
-          let datesObj = {};
-          let yAxis = ['Applied'];
-          let xAxis = [];
-
-          jobs.data.forEach((job) => {
-            let convertedDate = (new Date(job.created_at)).toDateString();
-            if (!datesObj[convertedDate]) {
-              datesObj[convertedDate] = 1;
-            } else {
-              datesObj[convertedDate] += 1;
-            }
-          });
-
-          for (let key in datesObj) {
-            yAxis.push(datesObj[key]);
-            xAxis.push(key);
-          }
-
-          // console.log(xAxis);
-          // console.log(yAxis);
-          context.setState({
-            barChartDates: xAxis,
-            barChartJobsApplied: yAxis,
-            jobsAppliedTo: jobs.data,
-            loaded: true
-          });
-
-
-          this.urlParser(context.state.allData, this.state.skills);
-        })
-        .catch(err => {
-          console.error('Error occured getting jobs', err);
-        });
-      }
-    });
-  }
-
-  urlParser(DataArray, skills) {
-    let context = this;
-    $.ajax({
-      type: 'POST',
-      url: '/urlParser',
-      contentType: 'application/JSON',
-      data: JSON.stringify({
-        data: DataArray,
-        skills: skills
-      })
-    })
-    .done((ranking) => {
-      ranking = JSON.parse(ranking);
-      context.setState({
-        keywordsRanking: ranking
-      })
-      if (context.state.keywordsRanking !== null) {
-        context.setState({
-          donutChart: true
-        })
-      }
-    })
-    .fail(err => {
-      console.error('Error occured getting jobs', err);
-    });
-  }
-
-  getResume() {
-    let context = this;
-    $.ajax({
-      type: 'GET',
-      url: '/user',
-      datatype: 'json'
-    })
-    .done((user) => {
-      $.ajax({
-        url: '/getResume',
-        type: 'GET',
-        data: {resume_id: user.resume_id},
-      })
-      .done((resume) => {
-        axios.get('/analyzeResume', {
-          params: {
-            url: resume.resume_url
-          }
-        })
-        .then((text) => {
-          // console.log('Inside analytics', text.data);
-        });
-        context.setState({
-          skills: resume.skills.split(','),
-          file: resume.resume_url
-        });
-      })
-      .fail(function(err) {
-        console.log('failed to GET', err);
-      });
-    })
-    .fail(function(err) {
-      console.log('failed to GET', err);
-    });
-  }
-
   render() {
     return (
       <div>
-        <h4>Keywords tracker</h4>
-        {this.state.donutChart ?<DonutChart ranking={this.state.keywordsRanking} /> : null}
         <div className="container-fluid">
           <div className="row">
+            
+            <div className="col-sm-6">
+              <Card>
+                <CardTitle title="Keyword Analysis" subtitle="Skills pulled from your resume" style={styles.cardTitle} titleColor="#000000" subtitleColor="#000000"/>
+                <CardMedia>
+                </CardMedia>
+                <CardText>
+                  <h4>Keywords tracker</h4>
+                  {this.state.donutChart ? <DonutChart ranking={this.state.keywordsRanking} /> : null}
+                </CardText>
+              </Card>
+            </div>
+
             <div className="col-sm-6">
               <Card>
                 <CardTitle title="IBM Watson Analysis" subtitle="Smart analysis based on your resume" style={styles.cardTitle} titleColor="#000000" subtitleColor="#000000"/>
@@ -379,6 +385,7 @@ class SmartAnalysis extends React.Component {
                 </CardText>
               </Card>
             </div>
+
           </div>
         </div>
       </div>
