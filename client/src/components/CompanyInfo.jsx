aaef999711b2ff24357cc4c67053fc5d51077ada
Jon Eric Escobedo <jonericescobedo@gmail.com>
2017-06-15 08:40:07 -0700

    (style) companyinfo styling upgraded

--- client/src/components/CompanyInfo.jsx
+++ client/src/components/CompanyInfo.jsx
@@ -5,6 +5,8 @@ import Dialog from 'material-ui/Dialog';
 import Snackbar from 'material-ui/Snackbar';
 import PercentChart from './C3Components/PercentChart.jsx';
 import Chip from 'material-ui/Chip';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import NavigationBack from 'material-ui/svg-icons/navigation/arrow-back';
 
 const styles = {
   card: {
@@ -15,7 +17,14 @@ const styles = {
   },
   wrapper: {
     display: 'flex',
-    flexWrap: 'wrap'
+    flexWrap: 'wrap',
+    paddingLeft: '25px'
+  },
+  snackbar: {
+    background: '#1DE9B6',
+  },
+  login: {
+    height: '200px'
   }
 };  
 
@@ -204,16 +213,20 @@ class CompanyInfo extends React.Component {
     ];
 
     return (
-      <div className="container wow fadeIn" data-wow-delay="0.2s">
-        {this.state.jobs.length && this.state.loaded === true ? this.state.jobs.map((job, i) => {
+      <div className="container wow fadeIn" data-wow-delay="0.6s">
+        <FloatingActionButton backgroundColor="#00BFA5" className="fab" zDepth="5" onClick={this.goBack}>
+          <NavigationBack />
+        </FloatingActionButton>
+        {this.state.jobs.length ? this.state.jobs.map((job, i) => {
           if (i === 0) {
             return (
               <div className="card-group">
                 
                 {/* Card one */}
                 <div className="card" style={styles.card}>
-                  <div className="equal-height card-block">
-                    <img className="img-fluid mx-auto d-block z-depth-3" src={job.squareLogo} alt={job.name} size="125"/>
+                  {/* Top of card */}
+                  <div className="equal-height" id="job-logo">
+                    <a target="_blank" href={'http://' + job.website}><img className="img-fluid mx-auto d-block z-depth-3" src={job.squareLogo} alt={job.name} size="125"/></a>
                   </div>
                   <hr/>
                   <div className="card-block">
@@ -221,14 +234,19 @@ class CompanyInfo extends React.Component {
                     <h6 className="card-text secondary-text">{jobInfo.jobtitle}</h6>
                     <p className="card-text secondary-text">{jobInfo.snippet}</p>
                     <a className="btn btn-primary" role="button" href={`${jobInfo.url}`} target="blank">Apply Now!</a>
-                    <a className="btn btn-default"><i className="fa fa-thumbs-up left"></i> Applied!</a>
+                    { this.props.loggedIn ?
+                      <a className="btn btn-default" onClick={this.appliedJob} onTouchTap={this.handleTouchTap}><i className="fa fa-thumbs-up left"></i> Applied!</a>
+                    :
+                      <a className="btn btn-default" onClick={this.handleOpen}><i className="fa fa-thumbs-up left"></i> Applied!</a>
+                    }
                     <p className="card-text disabled-text"><small className="text-muted">Posted {jobInfo.formattedRelativeTime}</small></p>
                   </div>
                 </div>
 
                 {/* Card two */}
                 <div className="card" style={styles.card}>
-                  <div className="equal-height card-block">
+                  {/* Top of card */}
+                  <div className="equal-height" id="job-ratings">
                     <p className="secondary-text">Overall Rating <span className="badge badge-primary badge-pill">{job.overallRating || 'N/A'}</span></p>
                     <p className="secondary-text">Work Life Balance <span className="badge badge-primary badge-pill">{job.workLifeBalanceRating || 'N/A'}</span></p>
                     <p className="secondary-text">Culture and Values <span className="badge badge-primary badge-pill">{job.cultureAndValuesRating || 'N/A'}</span></p>
@@ -244,28 +262,50 @@ class CompanyInfo extends React.Component {
                   </div>
                   <hr/>
                   <div className="card-block">
-                    <h4 className="card-title primary-text">{jobInfo.company}<span><h6><small className="text-muted">{jobInfo.formattedLocation}</small></h6></span></h4>
-                    <h6 className="card-text secondary-text">{jobInfo.jobtitle}</h6>
-                    <p className="card-text secondary-text">{jobInfo.snippet}</p>
-                    <a className="btn btn-primary" role="button" href={`${jobInfo.url}`} target="blank">Apply Now!</a>
-                    <a className="btn btn-default"><i className="fa fa-thumbs-up left"></i> Applied!</a>
-                    <p className="card-text disabled-text"><small className="text-muted">Posted {jobInfo.formattedRelativeTime}</small></p>
+                    <h4 className="card-title primary-text">Company ratings</h4>
+                    { job.numberOfRatings && job.ratingDescription ?
+                      <p className="card-text secondary-text">
+                        Located in {jobInfo.formattedLocation}, {jobInfo.company} is a company in the {job.industryName} industry. 
+                        Out of {job.numberOfRatings} Glassdoor.com reviews, {job.name} has an overall rating of {job.overallRating}, 
+                        leaving employees feeling &#34;{job.ratingDescription}&#34; with the company.
+                      </p>
+                      :
+                        <p className="disabled-text">This company hasn't been rated on Glassdoor yet.</p>
+                      }
                   </div>
                 </div>
 
                 {/* Card three */}
                 <div className="card" style={styles.card}>
-                  <div className="equal-height">
-                      <div style={styles.wrapper}>
-                        {this.state.progressChart ? <PercentChart data ={this.state.wordMatch} /> : null}
-                        {this.state.matchingSkills.map(this.renderChip, this)}
-                        <p className="card-text disabled-text"><small className="text-muted"></small></p>
-                      </div>
+                  {/* Top of card */}
+                  <div className="equal-height" id="skills-graph">
+                      {this.state.progressChart && this.props.loggedIn ? 
+                        <PercentChart data ={this.state.wordMatch} />
+                      : 
+                        <div className="row h-100">
+                          <div className="col-sm-12 my-auto">
+                            <p className="text-center disabled-text">Please log in to view graphs</p>
+                          </div>
+                        </div>
+                      }
                       </div>
                       <hr/>
                     <div className="card-block">
-                        <h4 className="card-title primary-text">Card title</h4>
-                        <p className="card-text secondary-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                        <h4 className="card-title primary-text">Skills match</h4>
+                        { this.props.loggedIn ?
+                          <p className="card-text secondary-text">
+                            After examining your resume and {jobInfo.company}'s posting, your resume matched
+                            the following skills:
+                          </p>
+                        :
+                          <p className="card-text secondary-text">
+                            Once you log in, we'll assess the skills on your resume with the skills required for the job!
+                          </p>
+                        }
+                        <div style={styles.wrapper}>
+                          {this.state.matchingSkills.map(this.renderChip, this)}
+                          <p className="card-text disabled-text"><small className="text-muted"></small></p>
+                        </div>
                     </div>
                 </div>
 
@@ -273,6 +313,22 @@ class CompanyInfo extends React.Component {
             );
           }
         }) : null}
+        <Dialog
+          title="Log in to continue"
+          actions={actions}
+          modal={false}
+          open={this.state.open}
+          onRequestClose={this.handleClose}
+        >
+          Please log in to use this feature.
+        </Dialog>
+        <Snackbar
+          open={this.state.openSnack}
+          message="You've applied to this job!"
+          autoHideDuration={4000}
+          onRequestClose={this.handleRequestClose}
+          bodyStyle={styles.snackbar}
+        />
       </div>
     );
   }
